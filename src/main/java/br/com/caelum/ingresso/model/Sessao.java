bf3c938fddbb0f99743b2c4b0111b79d43dfc85c
antoniel <atnbesson@gmail.com>
2019-05-25 10:38:17 -0300

    teste

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,14 +2,33 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@DateTimeFormat(pattern = "HH:mm")
+	@NotNull
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
 	
-	
+	public Sessao() {
+		
+	}
 	
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {		
 		this.horario = horario;
